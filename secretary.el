e71df41f6793e32e9a831594713f76d56b0d6cb7
Martin Edström <meedstrom@teknik.io>
2021-02-03 15:06:18 +0100

    Move stuff to a new secretary-config.el

--- secretary.el
+++ secretary.el
@@ -55,6 +55,8 @@
 
 (defvar secretary-debug-p t)
 
+(defvar secretary-queriers nil)
+
 (defgroup secretary nil "The Emacs in-house secretary."
   :prefix "secretary-"
   :group 'convenience)
@@ -262,24 +264,6 @@ max-entries-per-day, etc."
 ;;   (interactive)
 ;;   (secretary-do-query-maybe #'secretary-query-sleep nil t))
 
-(defvar secretary-queriers
-  (list (secretary-querier-create :fn #'secretary-query-sleep
-				  :log-file "/home/kept/Self_data/sleep.tsv"
-				  :min-hours-wait 5
-				  :use-posted t)
-	(secretary-querier-create :fn #'secretary-query-weight
-				  :log-file "/home/kept/Self_data/weight.tsv"
-				  :max-entries-per-day 1)
-	(secretary-querier-create :fn #'secretary-query-mood
-				  :log-file "/home/kept/Self_data/mood.tsv")
-	(secretary-querier-create :fn #'secretary-query-ingredients
-				  :log-file "/home/kept/Self_data/ingredients.tsv"
-				  :min-hours-wait 5)
-	(secretary-querier-create :fn #'secretary-query-cold-shower
-				  :log-file "/home/kept/Self_data/cold.tsv"
-				  :max-entries-per-day 1))
-    "To be customized by user.")
-
 (defvar secretary--inactive-queries-file
   "/home/kept/Emacs/secretary/pseudo-userdir/inactive-queries.el")
 
@@ -407,7 +391,7 @@ meant to get."
   (with-temp-buffer
     (insert-file-contents-literally path)
     (goto-char (point-max))
-    (when (looking-back "^")
+    (when (looking-back "^" nil)
       (forward-line -1))
     (search-forward "\t")
     (buffer-substring (point) (- (search-forward "\t") 1))))
@@ -438,7 +422,7 @@ meant to get."
   (with-temp-buffer
     (insert-file-contents path)
     (goto-char (point-max))
-    (when (looking-back "^") ;; if empty line
+    (when (looking-back "^" nil) ;; if empty line
       (forward-line -1))
     (split-string (buffer-substring (line-beginning-position)
 				    (line-end-position))
@@ -490,12 +474,12 @@ passed on to `call-process'."
 (defun secretary-idle-p ()
   (> (secretary--idle-seconds) secretary-idle-threshold))
 
-(defvar secretary--x11idle-program-name
+(defvar secretary-x11idle-program-name
   (or org-clock-x11idle-program-name "xprintidle"))
 
 (defun secretary--x11-idle-seconds ()
   "Like `org-x11-idle-seconds' without need for /bin/sh or org."
-  (/ (secretary--process-output-to-number secretary--x11idle-program-name) 1000))
+  (/ (secretary--process-output-to-number secretary-x11idle-program-name) 1000))
 
 ;; - Needs to be a function because something might kill the buffer.
 ;; - Can't use get-buffer-create because I want to turn on `visual-line-mode'.
@@ -574,65 +558,6 @@ begins on a newline."
 (defun secretary-change-date-yesterday ()
   (message "Applying this query AND subsequent queries for now to yesterday.")
   (setq secretary--date (ts-dec 'day 1 (ts-now))))
-
-(defvar secretary-aphorisms
-  '("The affairs of the world will go on forever. Do not delay the practice of meditation." ;; not koanish
-    "It takes all the running you can do, to keep in the same place."
-    "You can't hate yourself into someone that loves who they are."
-    "Don't show up to prove, show up to improve."
-    "If you are completely okay with ‘never doing’ and all the consequences which follow from never taking action, it’s easier to step into ‘always doing’."
-    ;; https://mxplx.com/schema/58/
-    "Life feeds on negentropy, sucking orderliness from its environment."
-    "The cosmos is just right intellectually, where things are knowable but endlessly challenging."
-    "Atoms are so small that if you poured a glass of water into the ocean and gave them time to distribute evenly, when you later took a glass from the ocean you would find a hundred of the original molecules within it."
-    "Big whorls have little whorls / Which feed on their velocity / And little whorls have lesser whorls, / And so on to viscosity."
-    "We live in a Universe where gravity pulls galaxies towards one another, but universal expansion pulls them apart."
-    "All life is made of lifeless sub-components below the level of the cell."
-    "An egg is a chemical process with the power to become a lifeless omelet or a living chicken."
-    "Everything is vibrations, light and other forms of radiation are roaming vibrations and matter is bottled up vibrations."
-    "The total energy of the Universe is Zero."
-    "The Cartesian Duality, the idea that our mind is separate from our matter, is an illusion."
-    "Light is both spread out and localized."
-    "Everything society builds and every social interaction we have with others is a plexus of motor habits."
-    "All matter is mostly empty space."
-    "The second law of thermodynamics as a profound distinction between past and future."
-    "The origin of the universe is ultimately unknowable."
-    ;; https://en.wikipedia.org/wiki/Koan
-    "If you meet the Buddha, kill the Buddha."
-    "One day as Manjusri stood outside the gate, the Buddha called to him, “Manjusri, Manjusri, why do you not enter?” Manjusri replied, “I do not see myself as outside. Why enter?”"
-    "Koan after koan explores the theme of nonduality. Hakuin's well-known koan, \"Two hands clap and there is a sound, what is the sound of one hand?\" is clearly about two and one. The koan asks, you know what duality is, now what is nonduality? In \"What is your original face before your mother and father were born?\" the phrase \"father and mother\" alludes to duality. This is obvious to someone versed in the Chinese tradition, where so much philosophical thought is presented in the imagery of paired opposites. The phrase \"your original face\" alludes to the original nonduality."
-    "Subject and object - this is two hands clapping. When the monk realizes that the koan is not merely an object of consciousness but is also he himself as the activity of seeking an answer to the koan, then subject and object are no longer separate and distinct [...] This is one hand clapping."
-    ;; https://www.greaterwrong.com/posts/3GZmttQJ4CRdMLsmJ/aphorisms-on-motivation
-    "All motivation ultimately stems from belief. If you don’t know why you want something, it stems from an alief that is hidden."
-    "Most of your hidden motivations will be hidden because they’re not socially desirable, and/or because they’re not consistent with your identity. I am not a paragon of virtue. I am just scared to own up to my vices, and it’s keeping me from integrating myself. The same is true for you and for everyone that isn’t completely enlightened."
-    "Your System 1 won’t stop complaining about that marshmallow as long as it’s in front of you. Eat it or throw it away, but don’t let it sit there slowly deconstructing your internal consistency."
-    "If you believe something is the right thing to do, but you can’t bring yourself to do it, then not all of you believes it."
-    "Aliefs will be ten times less reasonable if you shut them out. If you embrace them, only the most nuanced part of it remains."
-    "“Ego depletion” only makes sense if you don’t self-identify with your System 1. If you do, you will simply experience it as a preference."
-    "People are a bag of subagents. If you interact with them, your bags mix. If they offend you, that’s often because they’ve expressed a subagent that you repress in yourself. Your reaction is how you would react to this part of you internally."
-    ;; https://www.flightfromperfection.com/aphorisms-for-meditation.html
-    "Am I putting labels on feelings & thoughts? Am I spinning a story about these feelings & thoughts?"
-    "Am I imagining that this person actually is what I’m imagining them to be?"
-    "Am I thinking that whatever I’m thinking about is actually what I think it is?"
-    ;; https://www.greaterwrong.com/posts/n4ctAtDBNKnv78kxS/some-rationalistic-aphorisms
-    "If you never have akrasia, you’re spending too much time on trivial challenges."
-    "One of my goals lately has been to write a thousand words a day. I’ve noticed that the first three hundred are difficult but the last three thousand come naturally."
-    "I wish people would just update on the expectation of being Dutch booked due to inconsistent preferences and give me money."
-    "The feeling of knowing that one knows more than others is addicting and almost always wrong."
-    "Analyze verbs timelessly and nouns timefully. That way you look at the part of the conceptualization that isn’t already explicit in its construction."
-    "You can learn smart things from stupid people."
-    "Nothing is art, but anything can be treated as art."
-    "Adaptive is not optimal." ;; definitely koanish
-    "We all have the strength to refuse what we are not offered."
-    "You are what you fear to appear to be."
-    "Whatever you have done, you are the sort of person who would do that."
-    "Misspellers learn from speech, mispronouncers from books."
-    "The people are flattered more obsequiously than the monarch ever was."
-    "The incorruptible politician merely prefers power to money."
-    "Why do aphorisms and cynicism go together? A good single sentence saying can’t require background evidencing or further explanation. It must be instantly recognizable as true. It also needs to be news to the listener. Most single sentences that people can immediately verify as true they already believe. What’s left? Things that people don’t believe or think about much for lack of wanting to, despite evidence. Drawing attention to these is called cynicism."
-    ;; Atomic Habits
-    "Missing once is an accident, missing twice is the start of a new habit."
-    "If it happens once it's a mistake. If it happens twice, it's a choice."))
 
 
 ;;;; Greetings
@@ -714,11 +639,11 @@ of `secretary-greeting'. Mutually exclusive with
   (with-current-buffer buffer
     (whitespace-cleanup)
     (f-append-text (buffer-string) 'utf-8 path)
-    ;; TODO: delete region only if appending was successful
     (delete-region (point-min) (point-max))))
 
 ;; TODO: When buffer mode changes, count it as a new buffer. Note that (assoc
 ;; buf secretary-known-buffers) will still work.
+;; TODO: Optimize
 (defun secretary-log-buffer (&optional _arg)
   (unless (minibufferp)
     (let* ((buf (current-buffer))
@@ -770,7 +695,7 @@ of `secretary-greeting'. Mutually exclusive with
 ;;;###autoload
 (defun secretary-query-activity ()
   (interactive)
-  (when (interactive-p)
+  (when (called-interactively-p)
     (setq secretary--date (ts-now)))
   (let* ((name (secretary-read "What are you up to? " (secretary-activities-names))))
     (secretary-append-tsv "/home/kept/Self_data/activity.tsv"
@@ -1000,36 +925,6 @@ are minutes and numbers below are hours."
 
 ;; (define-key minibuffer-local-completion-map (kbd "C-o") #'secretary-special-handle-current-query)
 
-;; TODO: Merge with `secretary-read'
-(defun secretary-special-handle-current-query ()
-  (interactive)
-  (let ((input (completing-read "Yes? " '(
-					  "Never mind, let me reply normally."
-					  "Remind me about this one later."
-					  "Skip this."
-					  "Back to previous query."
-					  "Yes."
-					  "No."
-					  "Apply this query to a different date."
-					  "Apply this query to yesterday."
-					  "Tell me something profound."))))
-    (cond ((string-match-p (rx (or "remind" "later" "l8r" "resched")) input)
-           'reschedule)
-          ((string-match-p (rx (or "food" "ingred")) input)
-           (secretary-query-ingredients))
-          ((string-match-p (rx (or "profound")) input)
-           (secretary-emit (seq-random-elt secretary-aphorisms)))
-          ((string-match-p (rx (or "yesterday" "yday")) input)
-           (secretary-change-date-yesterday))
-          ((string-match-p (rx "date") input)
-           (secretary-change-date))
-          ((string-match-p (rx (or "nvm" "never mind" (seq bow "nm" eow))) input)
-           nil)
-          ((string-match-p (rx (or "exit" "quit" "ttyl" "bye")) input)
-           (keyboard-quit))
-          (t
-           (secretary-emit "Override not understood.")))))
-
 
 ;;;; Welcomers
 
@@ -1040,7 +935,7 @@ are minutes and numbers below are hours."
   (setq secretary--date (ts-now))
   (secretary--raise-frame-and-do
    (dolist (x secretary--active-queries)
-     (secretary-do-query-maybe x secretary--date (interactive-p)))))
+     (secretary-do-query-maybe x secretary--date (called-interactively-p)))))
 
 (defun secretary-call (&optional arg)
   "Call your secretary. Useful when you're unsure what to do."
@@ -1420,7 +1315,7 @@ Behavior indeterminate you've forced it on in several Emacsen."
 		    (fset 'secretary--idle-seconds #'org-mac-idle-seconds)
 		    t)
                    ((and (eq window-system 'x)
-                         (executable-find secretary--x11idle-program-name))
+                         (executable-find secretary-x11idle-program-name))
 		    (fset 'secretary--idle-seconds #'secretary--x11-idle-seconds)
 		    t)
                    (t
@@ -1433,7 +1328,16 @@ Behavior indeterminate you've forced it on in several Emacsen."
 		   (message "Another secretary active.")
 		   (secretary-mode 0)
 		   nil)
-	       t))
+	       t)
+	     (if (--all-p (and (boundp it)
+			       (not (null it)))
+			  '(secretary-aphorisms
+			    secretary-queriers))
+		 t
+	       (message "Needed variables not set, see manual or do %s."
+			"M-x load-library secretary-config")
+	       (secretary-mode 0)
+	       nil))
 	(mkdir "/tmp/secretary/" t)
 	(f-write (number-to-string (emacs-pid))
 	   'utf-8 "/tmp/secretary/pid")
