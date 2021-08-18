0c5d0b7c065dd1574b1d064f48ae5af164198ba2
Martin Edström <meedstrom@teknik.io>
2021-08-18 20:38:53 +0200

    Update licensing

--- make_data_for_plots.R
+++ make_data_for_plots.R
@@ -1,7 +1,7 @@
 # Copyright (C) 2020-2021 Martin Edström
 
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
@@ -10,7 +10,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 
-# You should have received a copy of the GNU Affero General Public License
+# You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 # ------------------------------------------------------------------------
--- mood.gnuplot
+++ mood.gnuplot
@@ -6,8 +6,8 @@ set timefmt "%Y-%m-%d"
 set format x "%b %d"
 set yrange [0:5]
 set xrange ["2021-01-01":]
-plot "/tmp/secretary/mood.dat" using 1:2 notitle
+plot "/tmp/eva/mood.dat" using 1:2 notitle
 
 # Use the mood_desc column as labels (not suitable for ascii).
-# plot "/tmp/secretary/mood.dat" using 1:2 notitle,\
+# plot "/tmp/eva/mood.dat" using 1:2 notitle,\
 #      "" using 1:2:3 notitle with labels
